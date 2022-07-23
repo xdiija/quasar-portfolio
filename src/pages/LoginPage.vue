054ba26cb5a48c118175e5d081bb32dff51b1ce7
xdiija <djalmaleandro@yahoo.com>
2022-07-23 17:23:57 -0300

    Adição de rota login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="text-h2">Login</p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,13 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
